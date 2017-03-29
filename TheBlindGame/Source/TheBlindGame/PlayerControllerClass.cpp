982edb27cbfc1432e9cf8aa4eec1f416b650a741
Smoki-PC\Smoki <matko.smoljan@gmail.com>
2017-03-29 15:06:53 +0200

    TBG02 Restricted camera to only rotate left or right

--- TheBlindGame/Source/TheBlindGame/PlayerControllerClass.cpp
+++ TheBlindGame/Source/TheBlindGame/PlayerControllerClass.cpp
@@ -10,5 +10,8 @@ void APlayerControllerClass::BeginPlay()
 	Super::BeginPlay();	// call default behavior before anything else
 
 	UE_LOG(LogTemp, Warning, TEXT("I am the new player controller!"));
-	
+
+	// dissable camera from looking up or down
+	PlayerCameraManager->ViewPitchMax = 0.0f;
+	PlayerCameraManager->ViewPitchMin = -0.0f;
 }
