1d28a39d7744500163e51893035ce5e09ea13086
Smoki-PC\Smoki <matko.smoljan@gmail.com>
2017-03-31 12:40:14 +0200

    TBG05 Player Line tracing to see if we can grab the chicken

--- TheBlindGame/Source/TheBlindGame/PlayerControllerClass.cpp
+++ TheBlindGame/Source/TheBlindGame/PlayerControllerClass.cpp
@@ -20,7 +20,67 @@ void APlayerControllerClass::BeginPlay()
 	
 }
 
+void APlayerControllerClass::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	Grab();
+}
+
 APawn * APlayerControllerClass::GetControlledPawn()
 {
 	return GetPawn();
+}
+
+void APlayerControllerClass::Grab()
+{
+	auto HitResult = GetFirstPhysicsBodyInReach();
+	auto ActorHit = HitResult.GetActor();
+
+	// If we hit the chicken with our LINE TRACE
+	if (ActorHit)
+	{
+		// Grab the chicken
+		UE_LOG(LogTemp, Warning, TEXT("We got the chicken!"));
+	}
+}
+
+const FHitResult APlayerControllerClass::GetFirstPhysicsBodyInReach()
+{
+	/// Line-trace (Ray-cast) out to reach distance
+	FHitResult HitResult;
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT HitResult,
+		GetReachLineStart(),
+		GetReachLineEnd(),
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+	return HitResult;
+}
+
+FVector APlayerControllerClass::GetReachLineStart()
+{
+	/// Get player view point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+	return PlayerViewPointLocation;
+}
+
+FVector APlayerControllerClass::GetReachLineEnd()
+{
+	/// Get player view point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+	return PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
 }
